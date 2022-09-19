829bbb60bb48dc676d5b512c1e7ddc556fd8db35
Jonna Hyypiä <jonna.hyypia@fi.experis.com>
2022-09-19 16:08:37 +0300

    Add limitation for history to 10 last translations

--- src/components/Profile/ProfileTranslationHistory.jsx
+++ src/components/Profile/ProfileTranslationHistory.jsx
@@ -2,12 +2,12 @@ import ProfileTranslationHistoryItem from "./ProfileTranslationHistoryItem"
 
 const ProfileTranslationHistory = ({ translations }) => {
 
-    const translationList = translations.map(
+    const translationList = translations.slice(-10).map(
         (translation, index) => <ProfileTranslationHistoryItem key={ index + "-" + translation } translation={ translation } />)
 
     return (
         <section>
-            <h2 className="translation_history">Your translation history</h2>
+            <h2 className="translation_history">Your translation history:</h2>
             { translationList.length === 0 && <p className="no_translations">You have no translations yet.</p>}
             <ul>{ translationList }</ul>
         </section>
